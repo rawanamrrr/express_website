aa3be01c0d25c082a1e9f76fcaf5651ae52077ef
rawanamrrr <rawanamr20002@icloud.com>
2025-11-20 02:43:22 +0200

    apply gitignore

--- .next/dev/static/chunks/app_about_page_tsx_6bece898._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_e6e3ad0e._.js",
-  "static/chunks/_76d5db75._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/app_contact_page_tsx_6bece898._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_9d136984._.js",
-  "static/chunks/_f3ead2f2._.js"
-],
-    source: "dynamic"
-});
