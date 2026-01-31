6b3f3f7105f6d214f111ad17fa8fe63ac3cfe88b
rawanamrrr <rawanamr20002@icloud.com>
2026-01-31 09:57:07 +0200

    app/page.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -55,12 +55,11 @@ export default function Hero() {
     '/hero 1.jpg',
     '/hero 2.jpg',
     '/hero 3.jpg',
-    '/hero 4.jpg',
+    '/hero 7.jpg',
     '/hero 5.jpg',
     '/hero 6.jpg',
-    '/hero 7.jpg',
+    '/hero 4.jpg',
     '/hero 8.jpg',
-    '/hero 9.jpeg',
     '/hero 10.jpg',
   ]
   const [current, setCurrent] = useState(0)
